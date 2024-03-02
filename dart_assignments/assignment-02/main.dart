ae8d86055821edc78bfed784f7d3c0ae73cf987a
muhammadhussain-code <muhammadhussain99100@gmail.com>
2024-03-02 19:45:01 +0500

    🐛 FIX: added questions

--- dart_assignments/assignment-02/main.dart
+++ dart_assignments/assignment-02/main.dart
@@ -1,5 +1,7 @@
 void main() {
-  // Q1
+  // Q.1: Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+  // i.e: if both values are equal then it's square otherwise rectangle.
+
   int length = 5;
   int breadth = 3;
 
@@ -9,7 +11,7 @@ void main() {
     print("It's a rectangle.");
   }
 
-  // Q2
+  // Q.2: Take two variables and store age then using if/else condition to determine oldest and youngest among them.
 
   int age1 = 30;
   int age2 = 25;
@@ -22,7 +24,12 @@ void main() {
     print("Both are the same age: $age1.");
   }
 
-  // Q3
+// Q.3: A student will not be allowed to sit in exam if his/her attendance is less than 75%. Create integer variables and assign value:
+// Number of classes held = 16,
+// Number of classes attended = 10,
+// and print percentage of class attended.
+// Is student is allowed to sit in exam or not?
+  
   int classesHeld = 16;
   int classesAttended = 10;
 
@@ -34,7 +41,11 @@ void main() {
     print("Student is not allowed to sit in the exam due to low attendance.");
   }
 
-  // Q4
+  
+  // Q.4: Create integer variable assign any year to it and check if a year is leap year or not.
+  // If a year is divisible by 4 then it is leap year but if the year is century year like 2000, 1900, 2100 then it must be divisible by 400.
+  // i.e: Use % ( modulus ) operator.
+
   int year = 2024;
 
   if (year % 4 == 0) {
@@ -43,7 +54,17 @@ void main() {
     print("$year is not a leap year.");
   }
 
-  //Q5
+
+// Q.5  Write a program to read temperature in centigrade and display a suitable message according to temperature:
+// You have num variable temperature = 42;
+// Now print the message according to temperature:
+// temp < 0 then Freezing weather
+// temp 0-10 then Very Cold weather
+// temp 10-20 then Cold weather
+// temp 20-30 then Normal in Temp
+// temp 30-40 then Its Hot
+// temp >=40 then Its Very Hot
+
   double temperature = 42;
 
   if (temperature < 0) {
@@ -60,7 +81,9 @@ void main() {
     print("It's Very Hot.");
   }
 
-  //Q6
+
+  // Q.6: Write a program to check whether an alphabet is a vowel or consonant.
+
   String alphabet = 'A';
 
   alphabet = alphabet.toLowerCase();
@@ -76,7 +99,27 @@ void main() {
     print("Consonant");
   }
 
-  //Q7
+
+// Q.7: Write a program to calculate and print the Electricity bill of a given customer. Create variable for customer id, name, unit consumed by the user, bill_amount and print the total amount the customer needs to pay. The charge are as follow :
+
+// Unit    Charge/unit
+// upto 199    @1.20
+// 200 and above but less than 400    @1.50
+// 400 and above but less than 600    @1.80
+// 600 and above             @2.00;
+
+// Test Data :
+// id: 1001
+// name: James
+// units: 800
+// Expected Output :
+// Customer IDNO :1001
+// Customer Name :James
+// unit Consumed :800
+// Amount Charges @Rs. 2.00 per unit : 1600.00
+// Net Bill Amount : 1600.00
+
+
   int customerId = 1001;
   String customerName = "James";
   int unitsConsumed = 800;
